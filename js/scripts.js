b42f033ab07d6f9b859a3e9bec15acc3535b10ac
Jason Spidle <jason@foundrymakes.com>
2016-09-16 01:11:22 -0500

    Adjust magnifying glass and add some animation to the background.

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-  $('.about-link').click(function(){
-    vex.dialog.alert('In the night of time our writhing track our trace an imprint leaves its body on the stone, its knowing. Mark x mark the falling and ourselves caught in the clay, the having stood in singular production. A body of work and its impetus, the invention of goad, both beast & attendant mobility. As every form of question is similarly ended with the same manner of dot, a gap, a line into parenthetical half-circumscription, the open-ended idea drives the effort of our point – the spot and the tip of the stylus coinciding at the nexus of what all has fashioned us to shape. By tooth, by claw, by will and fire we depart ourselves, drawn striven toward the core in this canvas, renewed.');
-  });
-  $('.submit-link').click(function(){
-    vex.dialog.alert('ICHNOS is published according to no guidelines, biases, proclivities or schedule by <a href="http://www.unwin-dunraven.com">Unwin-Dunraven Literary Ecclesia</a>. Email work to editors@ichnos.net');
-  });
-});
